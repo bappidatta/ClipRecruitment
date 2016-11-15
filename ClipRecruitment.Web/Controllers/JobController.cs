719cdc8d7a2e5def3a614e0e06a3b1a219900c52
Taz Uddin <getleton@gmail.com>
2016-11-15 15:27:22 +0600

    new controllers

---
+++ ClipRecruitment.Web/Controllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClipRecruitment.Web.Controllers
+{
+    public class JobController : ApiController
+    {
+        public IHttpActionResult GetAllJob()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHttpActionResult CreateJob()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ClipRecruitment.Web/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using ClipRecruitment.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClipRecruitment.Web.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Authenticate(User userVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Register(User userVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IHttpActionResult LogIn(User userVM)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
