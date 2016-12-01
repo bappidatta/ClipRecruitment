76e888e23f256edf4a1bb0519e3f01cbdc8f9822
Taz Uddin <getleton@gmail.com>
2016-12-01 16:57:12 +0600

    basic form validation

--- frontend/app/js/controllers/candidateSignupController.js
+++ frontend/app/js/controllers/candidateSignupController.js
@@ -1,61 +1,78 @@
-function candidateSignupController(commonService, candidateService, $location){
+function candidateSignupController(commonService, candidateService, $location) {
     'ngInject';
     const vm = this;
     vm.salaryRange = [10000, 20000, 30000, 40000, 50000, 60000];
 
-    vm.signUpModel = {  
-        FirstName : '',
-        Surname : '',
+    vm.signUpModel = {
+        FirstName: '',
+        Surname: '',
         CurrentSalary: '',
         IndustryList: []
     }
 
-    vm.signUp = function(){        
-        if(vm.candidateForm.$valid){
-            if(vm.signUpModel.IndustryList.length < 1){
+    vm.signUp = function () {
+        if (vm.candidateForm.$valid) {
+            if (vm.signUpModel.IndustryList.length < 1) {
                 // error
                 console.log('error');
-                return; 
-            }            
-            
-            candidateService.signUp(vm.signUpModel).then(function(res){
-                if(res.data.Success){
+                return;
+            }
+
+            candidateService.signUp(vm.signUpModel).then(function (res) {
+                if (res.data.Success) {
                     $location.path('/SignIn');
                 }
             });
 
 
-        }else{
+        } else {
             console.log('invalid');
         }
 
     }
 
-    vm.getPositions = function(viewValue){
-       if(viewValue != null && viewValue != ''){
-            return commonService.getLocations(viewValue).then(function(res){
-                return res.data.Success;                    
-        });
-       }
+    vm.getLocation = function (viewValue) {
+        if (viewValue != null && viewValue != '') {
+            return commonService.getLocations(viewValue).then(function (res) {
+                return res.data.Success;
+            });
+        }
+    }
+
+    vm.confirmEmail = function () {
+        if (vm.signUpModel.Email != vm.signUpModel.ConfirmEmail) {
+            vm.candidateForm.confirmEmail.$setValidity('comfirmEmail', false);
+        }
+        else {
+            vm.candidateForm.confirmEmail.$setValidity('comfirmEmail', true);
+        }
+    }
+
+    vm.confirmPassword = function () {
+        if (vm.signUpModel.Password != vm.signUpModel.ConfirmPassword) {
+            vm.candidateForm.confirmPassword.$setValidity('confirmPassword', false);
+        }
+        else {
+            vm.candidateForm.confirmPassword.$setValidity('confirmPassword', true);
+        }
     }
 
 
+    vm.resetForm = function () {
 
-    vm.resetForm = function(){
-        
     }
 
-    vm.toggleIndustry = function(isTrue, industry){
+    vm.toggleIndustry = function (isTrue, industry) {
         let index = vm.signUpModel.IndustryList.indexOf(industry);
-        if(isTrue && index < 0){            
+        if (isTrue && index < 0) {
             vm.signUpModel.IndustryList.push(industry);
-        }else{
+        } else {
             vm.signUpModel.IndustryList.splice(index, 1);
         }
     }
 
-    vm.empty = function(object){
-        for(var prop in object){
+    vm.empty = function (object) {
+        for (var prop in object) {
             object[prop] = '';
         }
         return object;
@@ -64,7 +81,7 @@ function candidateSignupController(commonService, candidateService, $location){
 
 
 
-export default{
+export default {
     name: 'candidateSignupController',
     fn: candidateSignupController
 }
